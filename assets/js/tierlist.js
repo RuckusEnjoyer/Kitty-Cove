392a2925588b2e0fad51ea4efd8c4c1ddcf49774
Kork <hail3yp@gmail.com>
2023-10-04 13:38:32 -0400

    added cat image fetch request, made cats spawn outside of the tierlist categories, added a title and a description to the minigame

--- assets/js/tierlist.js
+++ assets/js/tierlist.js
@@ -1,3 +1,5 @@
+//Functions that make the tierlist images drag/droppable
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -11,3 +13,32 @@ function dragDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+//TO DO: fetch cat pictures for tierlist:
+
+var apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=20'
+
+var catBtn = document.querySelector('#image-getter')
+
+function getImages(){
+    fetch(apiUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+            var imgEl = document.createElement('img');
+            var imageSpawner = document.querySelector('#image-spawner');
+            imgEl.src = data[i].url;
+    
+            imgEl.setAttribute('id', 'div' + [i] );
+            imgEl.setAttribute('class', 'tier-img');
+            imgEl.setAttribute('draggable', 'true');
+            imgEl.setAttribute('ondragstart', 'dragStart(event)');
+            imageSpawner.appendChild(imgEl);
+          }
+    })
+}
+
+catBtn.addEventListener('click', getImages)
