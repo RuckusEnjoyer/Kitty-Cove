90aa9c81e17d017ac403be6a3faac07ee95db7d7
Matthew Baka <matthewbaka@gmail.com>
2023-10-10 23:30:36 -0400

    added to sushi game

--- assets/js/sushi.js
+++ assets/js/sushi.js
@@ -1,13 +1,3 @@
-/*
-
-NOTE: not all sushis are on the same page
-will the js file run each time the page is loaded? (i think yes)
-will there be errors when not all sushis are found? (i think yes?)
-
-have a counter on the sushi page "5 sushis remaining"
-
-*/
-
 const totalSushi = 5;
 let sushiClicked = 0;
 
@@ -16,20 +6,32 @@ for (let i = 1; i <= totalSushi; i++) {
     //hides sushi if already clicked
     if (localStorage.getItem(`sushi-${i}`) === "eaten") {
         if (sushi) {
-            sushi.style.display = 'none';
+            sushi.style.display = "none";
         }
         sushiClicked++;
     }
     if (sushi) {
         sushi.addEventListener('click', function() {
             //hides sushi if clicked
-            sushi.style.display = 'none';
+            sushi.style.display = "none";
             localStorage.setItem(`sushi-${i}`, "eaten");
             sushiClicked++;
             if (sushiClicked === totalSushi) {
                 //we're done
             }
         })
+    }
+}
+
+sushiP = document.getElementById("sushi-p");
+if (sushiP) {
+    let sushisLeft = totalSushi - sushiClicked;
+    if (sushisLeft === 1) {
+        sushiP.innerText = "There is 1 sushi left";
+    } else if (sushisLeft === 0) {
+        sushiP.innerText = "You found all the sushis!";
+    } else {
+        sushiP.innerText = `There are ${sushisLeft} sushis left`;
     }
 }
 
